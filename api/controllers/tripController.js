6ac8b6b44b96a67446800e92335ae27cfc95743d
henry <siauhenry@gmail.com>
2018-04-03 00:13:50 -0700

    feat: enable sent invitation code by email function

--- api/controllers/tripController.js
+++ api/controllers/tripController.js
@@ -2,6 +2,16 @@ var Trip = require('../../models/tripModel').Trip;
 var User = require('../../models/userModel').User;
 var mongoose = require('mongoose');
 var config = require('../../config');
+var instanceConfig = require('../../instanceConfig');
+const nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: instanceConfig.gmailAddress,
+        pass: instanceConfig.gmailPassword
+    }
+});
 
 const strip = (str) => {
     if (str) {
@@ -219,25 +229,35 @@ exports.getTripInfo = function (req, res) {
 }
 
 exports.inviteMembers = function (req, res) {
-    //TODO implement
-    // This is only dummy resonse
-    if (req.body) {
-        if (req.body.inviteCode && req.body.emailList) {
-            // TODO: sent email with inviteCode
+    // TODO: add email server
+    if (req.body.invitationCode && req.body.emailList && req.body.tripId) {
+        const message = req.body.message ? req.body.message.replace(/\n/g, '<br />') : '';
+        let subject = req.body.subject;
+        const link = `<p>Click <a href="https://www.gtplanner.com/trip/join?code=${req.body.invitationCode}">here </a>to join the group</p>`;
+        const mailOptions = {
+            from: 'gtplanner.com@gmail.com', // sender address
+            to: req.body.emailList, // list of receivers
+            subject: subject, // Subject line
+            html: `<p>${message}</p><br />${link}`// body
+        };
+        transporter.sendMail(mailOptions, function (err, info) {
+            if (err)
+                console.log(err)
+            else
+                console.log(info);
             return res.status(200).json({
-                success: true
+                success: true,
+                numberOfEmails: req.body.emailList.length
             });
-        } else {
-            return res.status(200).json({
-                success: false
-            });
-        }
+        });
 
     } else {
-        return res.status(400).json({
-            success: false,
+        return res.status(200).json({
+            success: false
         });
     }
+
+
 }
 
 exports.getRecentTrips = function (req, res) {
